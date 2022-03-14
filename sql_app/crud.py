074e9e811f6b789e5e98b68b369f63c95ebf3a2b
MARIO FELIPE LUEVANO VILLAGOMEZ <mluevanov@gmail.com>
2022-03-14 04:48:53 -0600

    Correccion de estructura

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -15,23 +15,23 @@ def get_operation_by_symbol(db: Session, symbol: str):
 #     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-# def trade_symbol(db: Session, symbol: str, quantity: int, isbuy: bool):
-#     #check if symbol exists in nasdaq
+def trade_symbol(db: Session, symbol: str, quantity: int, isbuy: bool):
+    #check if symbol exists in nasdaq
+    
 
-
-#     operations_client = get_operation_by_symbol(db, symbol) 
-#     if operations_client is None:
+    operations_client = get_operation_by_symbol(db, symbol) 
+    if operations_client is None:
         
-#     if isbuy == True:
-#         operation = "buy"
-#     else:
-#         operation = "sell"
+    if isbuy == True:
+        operation = "buy"
+    else:
+        operation = "sell"
         
-#     db_trade = models.Operations(symbol=symbol, company_name=operations_client.company_name, price=operations_client.price, quantity=quantity, date=operations_client.date, profit_loss=operations_client.profit_loss, operation=operation, total_stock_value=operations_client.total_stock_value)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
+    db_trade = models.Operations(symbol=symbol, company_name=operations_client.company_name, price=operations_client.price, quantity=quantity, date=operations_client.date, profit_loss=operations_client.profit_loss, operation=operation, total_stock_value=operations_client.total_stock_value)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 # def get_items(db: Session, skip: int = 0, limit: int = 100):
 #     return db.query(models.Item).offset(skip).limit(limit).all()
 
